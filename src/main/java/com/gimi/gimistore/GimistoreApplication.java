b71760d3e64371d92922eb3259c3a4cda2a7038e
Le Chanh Tin <lechanhtin2000@gmail.com>
2023-03-04 18:02:47 +0700

    feat: Initial commit

---
+++ src/main/java/com/gimi/gimistore/GimistoreApplication.java
@@ -0,0 +1,13 @@
+package com.gimi.gimistore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimistoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimistoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gimi/gimistore/GimistoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimi.gimistore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimistoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
